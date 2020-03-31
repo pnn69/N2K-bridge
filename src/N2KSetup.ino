c49f07cd3ba324063a437c0870a12529a151b3bb
pnn69 <pnn_69@hotmail.com>
2020-03-31 16:52:06 +0200

    Noting really done here

--- src/N2KSetup.ino
+++ src/N2KSetup.ino
@@ -56,111 +56,110 @@ void IRAM_ATTR MOBtsk() { MOBactive = true; }
 /* To realise that kill them in the function ArduinoOTA.onStart */
 /**************************************************************************** */
 void setup_OTA() {
-  char buf[30];
-  byte mac[6];
-  Serial.println();
-  Serial.print("[SETUP] OTA...");
-  WiFi.macAddress(mac);
-  sprintf(buf, "%s-%02x:%02x:%02x:%02x:%02x:%02x", host, mac[0], mac[1], mac[2],
-          mac[3], mac[4], mac[5]);
-  ArduinoOTA.setHostname(buf);
-  ArduinoOTA.onStart([]() {
-    /* switch off all processes here!!!!! */
+    char buf[30];
+    byte mac[6];
     Serial.println();
-    Serial.println("Recieving new firmware now!");
-  });
-  ArduinoOTA.onEnd([]() {
+    Serial.print("[SETUP] OTA...");
+    WiFi.macAddress(mac);
+    sprintf(buf, "%s-%02x:%02x:%02x:%02x:%02x:%02x", host, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    ArduinoOTA.setHostname(buf);
+    ArduinoOTA.onStart([]() {
+        /* switch off all processes here!!!!! */
+        Serial.println();
+        Serial.println("Recieving new firmware now!");
+    });
+    ArduinoOTA.onEnd([]() {
     /* do stuff after update here!! */
-    Serial.println("\nRecieving done!");
-    Serial.println("Storing in memory and reboot!");
-    Serial.println();
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) { ESP.restart(); });
-  /* setup the OTA server */
-  ArduinoOTA.begin();
-  Serial.println("...done!");
+        Serial.println("\nRecieving done!");
+        Serial.println("Storing in memory and reboot!");
+        Serial.println();
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) { ESP.restart(); });
+    /* setup the OTA server */
+    ArduinoOTA.begin();
+    Serial.println("...done!");
 }
 
 /*****************************************************************************/
 /*Setup*/
 /*****************************************************************************/
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Booting");
-  pinMode(CANtx, OUTPUT);
-  pinMode(CANrx, INPUT);
-  pinMode(MOB, INPUT);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  TimeStamp = millis();
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  touchAttachInterrupt(T8, gotTouch1,
-                       threshold);  // depends on version sdk use T8 or T9
-  touchAttachInterrupt(T7, gotTouch2, threshold);
-  touchAttachInterrupt(T6, gotTouch3, threshold);
-  attachInterrupt(MOB, MOBtsk, FALLING);
-  setup_OTA();
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  TimeStamp = millis();
-  analogSetCycles(100);
-  touch1detected = false;
-  touch2detected = false;
-  touch3detected = false;
+    Serial.print("Hello world");
+    Serial.begin(115200);
+    Serial.println("Booting");
+    pinMode(CANtx, OUTPUT);
+    pinMode(CANrx, INPUT);
+    pinMode(MOB, INPUT);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    TimeStamp = millis();
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("Connection Failed! Rebooting...");
+        delay(5000);
+        ESP.restart();
+    }
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    touchAttachInterrupt(T8, gotTouch1, threshold);  // depends on version sdk use T8 or T9
+    touchAttachInterrupt(T7, gotTouch2, threshold);
+    touchAttachInterrupt(T6, gotTouch3, threshold);
+    attachInterrupt(MOB, MOBtsk, FALLING);  // init MOB interrupt
+    setup_OTA();
+    Serial.println("Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    TimeStamp = millis();
+    touch1detected = false;  // set touch to fase after start up
+    touch2detected = false;
+    touch3detected = false;
 }
 
 void loop() {
-  ArduinoOTA.handle();
-  if (TimeStamp + 500 < millis()) {
-    TimeStamp = millis();
-    Serial.println((analogRead(Vin) * 3.6 / 4095) * 5.7);
-    Serial.println(digitalRead(MOB));
+    ArduinoOTA.handle();
+    if (TimeStamp + 500 < millis()) {
+        TimeStamp = millis();
+        Serial.println((analogRead(Vin) * 3.6 / 4095) * 5.7);
+        Serial.println(digitalRead(MOB));
 
-    if (leds[0].green > 0) {
-      leds[0].green = 0;
-    } else {
-      leds[0].green = 5;
+        if (leds[0].green > 0) {
+        leds[0].green = 0;
+        } else {
+            leds[0].green = 5;
+        }
+        if (cnt) {
+            cnt--;
+        } else {
+            leds[1].red = 0;
+        }
+        FastLED.show();
     }
-    if (cnt) {
-      cnt--;
-    } else {
-      leds[1].red = 0;
+    
+    if (MOBactive == true) {
+        cnt = 3;
+        leds[1].red = 255;
+        FastLED.show();
+        MOBactive = false;
     }
-    FastLED.show();
-  }
-  if (MOBactive == true) {
-    cnt = 3;
-    leds[1].red = 255;
-    FastLED.show();
-    MOBactive = false;
-  }
 
-  if (touch1detected) {
-    touch1detected = false;
-    Serial.println("Touch 1 detected");
-    leds[1] = CRGB(0, 0, 5);
-    FastLED.show();
-  }
-  if (touch2detected) {
-    touch2detected = false;
-    Serial.println("Touch 2 detected");
-    leds[1] = CRGB(00, 5, 0);
-    FastLED.show();
-  }
-  if (touch3detected) {
-    touch3detected = false;
-    Serial.println("Touch 3 detected");
-    leds[1] = CRGB(5, 0, 0);
-    FastLED.show();
-  }
+    if (touch1detected) {
+        touch1detected = false;
+        Serial.println("Touch 1 detected");
+        leds[1] = CRGB(0, 0, 5);
+        FastLED.show();
+    }
+    if (touch2detected) {
+        touch2detected = false;
+        Serial.println("Touch 2 detected");
+        leds[1] = CRGB(00, 5, 0);
+        FastLED.show();
+    }
+  
+    if (touch3detected) {
+        touch3detected = false;
+        Serial.println("Touch 3 detected");
+        leds[1] = CRGB(5, 0, 0);
+        FastLED.show();
+    }
 }
